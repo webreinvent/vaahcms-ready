e2fafc48529f8eb001c4bb1de3752d3023162a5d
Pradeep <webreinvent@gmail.com>
2021-01-08 23:12:10 +0530

    Tested: Vaahcms setup

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        WebReinvent\VaahExtend\VaahExtendServiceProvider::class,
         WebReinvent\VaahCms\VaahCmsServiceProvider::class,
 
 
