011a231f9d3915571e28fd36a94c79c6e1376829
Pradeep <webreinvent@gmail.com>
2021-01-08 22:54:41 +0530

    Added: sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -37,4 +37,15 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+
+    public function report(Throwable $exception)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
+        parent::report($exception);
+    }
+
 }
