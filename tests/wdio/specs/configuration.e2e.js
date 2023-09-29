4fdd9347ce817cd92a65c73426fc34f56504b7bb
Abhijeet Ghosh <abhijeet-g001@webreinvent.com>
2023-09-29 19:05:50 +0530

    Updated: configuration.js | configuration.e2e.js
    Added all the test script for the configuration page.

--- tests/wdio/specs/configuration.e2e.js
+++ tests/wdio/specs/configuration.e2e.js
@@ -9,41 +9,42 @@ params.group = Data.groups[0];
 
 describe(Page.groupId(params), () => {
 
-    // params.test = Data.groups[0].tests[0];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[0].tests[0];
-    //     await Page.open();
-    //     await assert.pageUrl(inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[0].tests[1];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[0].tests[1];
-    //     await Page.open();
-    //     await assert.pageTitle(inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[0].tests[2];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[0].tests[2];
-    //     await Page.open();
-    //     await Page.assertTestDatabaseButton(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[0].tests[3];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[0].tests[3];
-    //     await Page.open();
-    //     await Page.assertTestMailButton(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[0].tests[4];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[0].tests[4];
-    //     await Page.open();
-    //     await Page.assertTestSaveButton(Data, inputs.assert);
-    // })
+    params.test = Data.groups[0].tests[0];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[0].tests[0];
+        await Page.open();
+        await assert.pageUrl(inputs.assert);
+    })
 
+    params.test = Data.groups[0].tests[1];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[0].tests[1];
+        await Page.open();
+        await assert.pageTitle(inputs.assert);
+    })
+
+    params.test = Data.groups[0].tests[2];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[0].tests[2];
+        await Page.open();
+        await Page.assertTestDatabaseButton(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[0].tests[3];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[0].tests[3];
+        await Page.open();
+        await Page.assertTestMailButton(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[0].tests[4];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[0].tests[4];
+        await Page.open();
+        await Page.assertTestSaveButton(Data, inputs.assert);
+    })
+
+    params.group = Data.groups[1];
     params.test = Data.groups[1].tests[0];
     it(Page.testId(params), async () => {
         inputs = Data.groups[1].tests[0];
@@ -100,120 +101,186 @@ describe(Page.groupId(params), () => {
         await Page.invalidDatabaseUsername(Data, inputs.assert);
     })
 
-    // params.test = Data.groups[2].tests[0];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[0];
-    //     await Page.open();
-    //     await Page.databaseButtonBlankTest(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[1];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[1];
-    //     await Page.open();
-    //     await Page.databaseButtonValidTest(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[2];
-    // it(Page.testId(params), async () => {
-    //     await Page.open();
-    //     await Page.testMailButtonFunctionality(Data);
-    // })
-    //
-    // params.test = Data.groups[2].tests[3];
-    // it(Page.testId(params), async () => {
-    //     await Page.open();
-    //     await Page.mailUsernameCloseButton(Data);
-    // })
-    //
-    // params.test = Data.groups[2].tests[4];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[4];
-    //     await Page.open();
-    //     await Page.envDropdownTest(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[5];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[5];
-    //     await Page.open();
-    //     await Page.debugDropdownTest(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[6];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[6];
-    //     await Page.open();
-    //     await Page.timezoneDropdownTest(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[7];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[7];
-    //     await Page.open();
-    //     await Page.databaseTypeDropdownTest(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[8];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[8];
-    //     await Page.open();
-    //     await Page.mailProviderDropdownTest(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[9];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[9];
-    //     await Page.open();
-    //     await Page.mailEncryptionDropdownText(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[10];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[10];
-    //     await Page.open();
-    //     await Page.envDropdownTest(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[11];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[11];
-    //     await Page.open();
-    //     await Page.mysqlDatabaseTest(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[12];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[12];
-    //     await Page.open();
-    //     await Page.postgresqlDatabaseTest(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[13];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[13];
-    //     await Page.open();
-    //     await Page.sqliteDatabaseTest(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[14];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[14];
-    //     await Page.open();
-    //     await Page.sqlServerDatabaseTest(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[15];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[15];
-    //     await Page.open();
-    //     await Page.databasePasswordEyeIconFunctionality(Data, inputs.assert);
-    // })
-    //
-    // params.test = Data.groups[2].tests[16];
-    // it(Page.testId(params), async () => {
-    //     inputs = Data.groups[2].tests[16];
-    //     await Page.open();
-    //     await Page.mailPasswordEyeIconFunctionality(Data, inputs.assert);
-    // })
+    params.group = Data.groups[2];
+    params.test = Data.groups[2].tests[0];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[0];
+        await Page.open();
+        await Page.databaseButtonBlankTest(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[1];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[1];
+        await Page.open();
+        await Page.databaseButtonValidTest(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[2];
+    it(Page.testId(params), async () => {
+        await Page.open();
+        await Page.testMailButtonFunctionality(Data);
+    })
+
+    params.test = Data.groups[2].tests[3];
+    it(Page.testId(params), async () => {
+        await Page.open();
+        await Page.mailUsernameCloseButton(Data);
+    })
+
+    params.test = Data.groups[2].tests[4];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[4];
+        await Page.open();
+        await Page.envDropdownTest(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[5];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[5];
+        await Page.open();
+        await Page.debugDropdownTest(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[6];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[6];
+        await Page.open();
+        await Page.timezoneDropdownTest(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[7];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[7];
+        await Page.open();
+        await Page.databaseTypeDropdownTest(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[8];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[8];
+        await Page.open();
+        await Page.mailProviderDropdownTest(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[9];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[9];
+        await Page.open();
+        await Page.mailEncryptionDropdownText(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[10];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[10];
+        await Page.open();
+        await Page.envFileNameTextbox(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[11];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[11];
+        await Page.open();
+        await Page.mysqlDatabaseTest(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[12];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[12];
+        await Page.open();
+        await Page.postgresqlDatabaseTest(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[13];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[13];
+        await Page.open();
+        await Page.sqliteDatabaseTest(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[14];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[14];
+        await Page.open();
+        await Page.sqlServerDatabaseTest(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[15];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[15];
+        await Page.open();
+        await Page.databasePasswordEyeIconFunctionality(Data, inputs.assert);
+    })
+
+    params.test = Data.groups[2].tests[16];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[2].tests[16];
+        await Page.open();
+        await Page.mailPasswordEyeIconFunctionality(Data, inputs.assert);
+    })
+
+    params.group = Data.groups[3];
+    params.test = Data.groups[3].tests[0];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[3].tests[0];
+        await Page.open();
+        await Page.validDataResponse(Data, inputs.assert);
+    })
+
+    params.group = Data.groups[3];
+    params.test = Data.groups[3].tests[1];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[3].tests[1];
+        await Page.open();
+        await Page.blankDataResponse(Data, inputs.assert);
+    })
+
+    params.group = Data.groups[3];
+    params.test = Data.groups[3].tests[2];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[3].tests[2];
+        await Page.open();
+        await Page.customOptionPageResponse(Data, inputs.assert);
+    })
+
+    params.group = Data.groups[3];
+    params.test = Data.groups[3].tests[3];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[3].tests[3];
+        await Page.open();
+        await Page.developOptionPageResponse(Data, inputs.assert);
+    })
+
+    params.group = Data.groups[3];
+    params.test = Data.groups[3].tests[4];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[3].tests[4];
+        await Page.open();
+        await Page.stagingOptionPageResponse(Data, inputs.assert);
+    })
+
+    params.group = Data.groups[3];
+    params.test = Data.groups[3].tests[5];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[3].tests[5];
+        await Page.open();
+        await Page.productionOptionPageResponse(Data, inputs.assert);
+    })
+
+    params.group = Data.groups[3];
+    params.test = Data.groups[3].tests[6];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[3].tests[6];
+        await Page.open();
+        await Page.wdiojsOptionPageResponse(Data, inputs.assert);
+    })
+
+    params.group = Data.groups[3];
+    params.test = Data.groups[3].tests[7];
+    it(Page.testId(params), async () => {
+        inputs = Data.groups[3].tests[7];
+        await Page.open();
+        await Page.blankAppNameResponse(Data, inputs.assert);
+    })
+
 })
